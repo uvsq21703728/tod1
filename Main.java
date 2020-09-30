ff561e40e7b7bd6589a4eb5324c0ae5a074dcb9c
ahmed.djeblahi <ahmed.djeblahi@ens.uvsq.fr>
2020-09-30 18:23:27 +0200

    main

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+fraction f = new fraction (5,6);
+System.out.println(fraction.toString(f));
+    
+}
---
+++ fraction.java
@@ -0,0 +1,6 @@
+package fr .uvsq.demo;
+public class fraction {
+    public static void main(String args[] ){
+        System.out.println("Hello");
+    }
+}
